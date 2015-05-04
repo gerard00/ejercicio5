9d38743a1b7dabbc4e855056f9228220f5478043
pc <pc@192.168.1.35>
2015-05-04 18:46:27 +0200

    prueba rama

--- src/javaapplication31/JavaApplication31.java
+++ src/javaapplication31/JavaApplication31.java
@@ -17,6 +17,9 @@ public class JavaApplication31 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola");
+        // esto es un cambio 
+        // y se muestra en verde
+        
     }
     
 }
